81b6173b91c04ce550534d58d0bc8c1886fc698b
scharon <jolyscharon@gmail.com>
2021-10-07 00:17:36 +0200

    Connecting to our Database

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+
+const PORT = 3030;
+const app = express(); 
+const todoRoutes = require("./routes/TodoRoutes");
+
+mongoose.connect("mongodb://localhost/todolist")
+    .then(() => console.log("connected successfully"))
+    .catch((err) => console.error(err));
+
+app.use("/todo", todoRoutes)// /todo/
+
+app.listen(PORT, () => {
+    console.log("The server is listeneing on port", PORT);
+});
